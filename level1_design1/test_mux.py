1681e9535820ef8a841d8cfb72c35d74d8b7b193
Prajvalsv <prajvals.ec19@bmsce.ac.in>
2022-07-23 16:38:22 +0000

    change_3

--- level1_design1/test_mux.py
+++ level1_design1/test_mux.py
@@ -8,41 +8,39 @@ import random
 async def test_mux(dut):
     """Test for mux2"""
       # input driving
-    dut.inp0 = 2;
-    dut.inp1 = 2;
-    dut.inp2 = 2;
-    dut.inp3 = 2;
-    dut.inp4 = 2;
-    dut.inp5 = 2;
-    dut.inp6 = 2;
-    dut.inp7 = 2;
-    dut.inp8 = 2;
-    dut.inp9 = 2;
-    dut.inp10 = 2;
-    dut.inp11 = 2;
-    dut.inp12 = 2;
-    dut.inp13 = 2;
-    dut.inp14 = 2;
-    dut.inp15 = 2;
-    dut.inp16 = 2;
-    dut.inp17 = 2;
-    dut.inp18 = 2;
-    dut.inp19 = 2;
-    dut.inp20 = 2;
-    dut.inp21 = 2;
-    dut.inp22 = 2;
-    dut.inp23 = 2;
-    dut.inp24 = 2;
-    dut.inp25 = 2;
-    dut.inp26 = 2;
-    dut.inp27 = 2;
-    dut.inp28 = 2;
-    dut.inp29 = 2;
-    dut.inp30 = 2;
+    dut.inp0.value = 2;
+    dut.inp1.value = 2;
+    dut.inp2.value = 2;
+    dut.inp3.value = 2;
+    dut.inp4.value = 2;
+    dut.inp5.value = 2;
+    dut.inp6.value = 2;
+    dut.inp7.value = 2;
+    dut.inp8.value = 2;
+    dut.inp9.value = 2;
+    dut.inp10.value = 2;
+    dut.inp11.value = 2;
+    dut.inp12.value = 2;
+    dut.inp13.value = 2;
+    dut.inp14.value = 2;
+    dut.inp15.value = 2;
+    dut.inp16.value = 2;
+    dut.inp17.value = 2;
+    dut.inp18.value = 2;
+    dut.inp19.value = 2;
+    dut.inp20.value = 2;
+    dut.inp21.value = 2;
+    dut.inp22.value = 2;
+    dut.inp23.value = 2;
+    dut.inp24.value = 2;
+    dut.inp25.value = 2;
+    dut.inp26.value = 2;
+    dut.inp27.value = 2;
+    dut.inp28.value = 2;
+    dut.inp29.value = 2;
+    dut.inp30.value = 2;
     for i in range(35):
-  dut.sel.value = random.randint(0, 31)
+      dut.sel.value = random.randint(0, 31)
         await Timer(2, units='ns')
         assert dut.out.value == 2, "Randomised test failed with: {}  = {}".format(dut.sel.value, dut.out.value)
 
-
-    cocotb.log.info('##### CTB: Develop your test here ########')
