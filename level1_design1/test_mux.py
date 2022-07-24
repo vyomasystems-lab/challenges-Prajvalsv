89495d95b61156cbd5368f25f98b8e10498fd76b
Prajvalsv <prajvals.ec19@bmsce.ac.in>
2022-07-24 08:33:00 +0000

    c

--- level1_design1/test_mux.py
+++ level1_design1/test_mux.py
@@ -53,7 +53,7 @@ async def test1_mux(dut):
    
    """Test for mux2_mytest"""
 
- # input driving
+      # input driving 
     dut.inp12.value = 1;
     dut.inp13.value = 2;
     dut.inp11.value = 3;
