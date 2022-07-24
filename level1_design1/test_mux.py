680c7ba43a44c9ddb3b66e4a28624aaf2ecda9fc
Prajvalsv <prajvals.ec19@bmsce.ac.in>
2022-07-24 08:24:55 +0000

    c

--- level1_design1/test_mux.py
+++ level1_design1/test_mux.py
@@ -39,7 +39,7 @@ async def test_mux(dut):
     dut.inp28.value = 2;
     dut.inp29.value = 2;
     dut.inp30.value = 2;
-    for i in range(35):
+    for i in range(4):
       dut.sel.value = random.randint(0, 31)
         assert dut.out.value == 2, f"Randomised test failed with: {} = {}".format(dut.sel.value, dut.out.value)
 
