ad33f68802d9570a64366bce0b2881147c24605c
Prajvalsv <prajvals.ec19@bmsce.ac.in>
2022-07-25 09:43:39 +0000

    c

--- level1_design2/test_seq_detect_1011.py
+++ level1_design2/test_seq_detect_1011.py
@@ -29,12 +29,11 @@ async def test_seq_bug1(dut):
        dut.inp_bit.value=0
        dut.inp_bit.value=1
        dut.inp_bit.value=0
-        dut.inp_bit.value=1
-        dut.inp_bit.value=1
+       dut.inp_bit.value=1
+       dut.inp_bit.value=1
        
-       
+        await Timer(2, units='ns')
    
-    assert dut.seq_seen.value == 1, "Randomised test failed with: {}  = {}".format(dut.inp_bit.value, dut.seq_seen.value)
+      assert dut.seq_seen.value == 1, "Randomised test failed with: {}  = {}".format(dut.inp_bit.value, dut.seq_seen.value)
 
 
-    cocotb.log.info('#### CTB: Develop your test here! ######')
