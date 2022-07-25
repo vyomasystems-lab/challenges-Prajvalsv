9ebfa98db26621a33e87212c2d15bb2df0c9e9cb
Prajvalsv <prajvals.ec19@bmsce.ac.in>
2022-07-25 10:11:34 +0000

    m

--- level1_design2/test_seq_detect_1011.py
+++ level1_design2/test_seq_detect_1011.py
@@ -27,6 +27,8 @@ async def test_seq_bug1(dut):
      
      for i in range(30):
         dut.inp_bit.value = random.randint(0,1)
+        await FallingEdge(dut.clk)
+
         assert dut.seq_seen.value == 1, "Randomised test failed with: {}  = {}".format(dut.inp_bit.value, dut.seq_seen.value)
 
 
