dade328d58f5a7c1dfb68c97b2012631b97f08a5
Prajvalsv <prajvals.ec19@bmsce.ac.in>
2022-07-25 10:20:30 +0000

    m

--- level1_design2/test_seq_detect_1011.py
+++ level1_design2/test_seq_detect_1011.py
@@ -26,13 +26,13 @@ async def test_seq_bug1(dut):
       # input driving
      
      for i in range(4):
-        dut.inp_bit.value = random.randint(0,1)
+        dut.inp_bit.value = 1
         await FallingEdge(dut.clk)
-        dut.inp_bit.value = random.randint(0,1)
+        dut.inp_bit.value = 0
         await FallingEdge(dut.clk)
-        dut.inp_bit.value = random.randint(0,1)
+        dut.inp_bit.value = 1
         await FallingEdge(dut.clk)
-        dut.inp_bit.value = random.randint(0,1)
+        dut.inp_bit.value = 1
         await FallingEdge(dut.clk)
         assert dut.seq_seen.value == 1, "Randomised test failed with: {}  = {}".format(dut.inp_bit.value, dut.seq_seen.value)
 
