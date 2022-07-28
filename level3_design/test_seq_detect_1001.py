174ccff4a55e0b1e994ec388e6c4228969623af7
Prajvalsv <109435612+Prajvalsv@users.noreply.github.com>
2022-07-29 01:23:45 +0530

    Create test_seq_detect_1001.py

---
+++ level3_design/test_seq_detect_1001.py
@@ -0,0 +1,39 @@
+# See LICENSE.vyoma for details
+
+# SPDX-License-Identifier: CC0-1.0
+
+import os
+import random
+from pathlib import Path
+
+import cocotb
+from cocotb.clock import Clock
+from cocotb.triggers import RisingEdge, FallingEdge
+
+@cocotb.test()
+async def test_seq_bug1(dut):
+     """Test for seq detection """
+
+     clock = Clock(dut.clk, 10, units="us")  # Create a 10us period clock on port clk
+     cocotb.start_soon(clock.start())        # Start the clock
+
+     # reset
+     dut.reset.value = 1
+     await FallingEdge(dut.clk)  
+     dut.reset.value = 0
+     await FallingEdge(dut.clk)
+
+      # input driving
+     
+     for i in range(4):
+        dut.inp_bit.value = 1
+        await FallingEdge(dut.clk)
+        dut.inp_bit.value = 0
+        await FallingEdge(dut.clk)
+        dut.inp_bit.value = 0
+        await FallingEdge(dut.clk)
+        dut.inp_bit.value = 1
+        await FallingEdge(dut.clk)
+        assert dut.seq_seen.value == 1, "Randomised test failed with: {}  = {}".format(dut.inp_bit.value, dut.seq_seen.value)
+
+
